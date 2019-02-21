391789faff4b83419f1315376147a25a7fae5a3c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-21 15:51:36 -0500

    Model completed

--- knexfile.js
+++ knexfile.js
@@ -3,19 +3,19 @@ const pg = require("pg");
 pg.defaults.ssl = true;
 
 module.exports = {
-  // development: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: "./data/dev.db3"
-  //   },
-  //   migrations: {
-  //     directory: "./data/migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds"
-  //   },
-  //   useNullAsDefault: true
-  // },
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/dev.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true
+  },
 
   production: {
     client: "pg",
