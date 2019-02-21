eba84871718e86eba97757952aff4d4e513e7e7b
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-21 16:07:20 -0500

    Model completed

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,7 @@ pg.defaults.ssl = true;
 module.exports = {
   development: {
     client: "pg",
-    connection:
-      "postgres://wniisgorwnahqa:3bcd0a3d4df2559871b7a95d9d34812b0cbd0ca45e7e0b63ba3d67fbf262cc36@ec2-54-225-237-84.compute-1.amazonaws.com:5432/dcf4018mto3p33",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -17,19 +16,4 @@ module.exports = {
     },
     useNullAsDefault: true
   }
-
-  // production: {
-  //   client: "pg",
-  //   connection:
-  //     "postgres://wniisgorwnahqa:3bcd0a3d4df2559871b7a95d9d34812b0cbd0ca45e7e0b63ba3d67fbf262cc36@ec2-54-225-237-84.compute-1.amazonaws.com:5432/dcf4018mto3p33",
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations",
-  //     directory: "./data/migrations"
-  //   },
-  //   useNullAsDefault: true
-  // }
 };
